3c9d5a2a15a6550a9eb81b761ddf2ca8743f7ed1
Gangadhar B <gangadharbavi312004@gmail.com>
2026-02-18 20:06:41 +0530

    Update and rename gets.c to input_output_formatted.c

--- C_basic_code_snippets/gets.c
+++
@@ -1,8 +0,0 @@
-#include<stdio.h>
-int main(){
-    char name[20];
-    printf("Enter your name: ");
-    gets(name);
-    printf("Hello, %s!", name);
-    return 0;
-}
---
+++ C_basic_code_snippets/input_output_formatted.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+int main(){
+    char a[10],b[10],c[10];
+    printf("Enter a : ");
+    scanf("%s",a);
+    printf("Enter b : ");
+    scanf("%s", b);
+    printf("The names are as follows:\n");
+    printf("a : %s\nb : %s\n ",a,b);
+    return 0;
+}
