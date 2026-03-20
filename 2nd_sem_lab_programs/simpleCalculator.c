83f02b01f43d58bc05ee6946b99a78f8189b7e3b
gangadharbhavi-7 <gangadharbavi312004@gmail.com>
2026-03-20 13:04:15 +0530

    add simulation of simple calculator with basic operators

--- 2nd_sem_lab_programs/simpleCalculator.c
+++ 2nd_sem_lab_programs/simpleCalculator.c
@@ -1,1 +1,43 @@
-xys
+#include<stdio.h>
+int main(){
+    char operator;
+    double num1, num2;
+    printf("Enter an operator(+,-,*,/,%%): ");
+    scanf("%c", &operator);
+    printf("Enter two numbers: ");
+    scanf("%lf %lf", &num1, &num2);
+    switch(operator){
+        case '+':
+        printf("%.1lf + %.1lf = %.1lf", num1, num2, num1 + num2);
+        break;
+        case '-':
+        printf("%.1lf- %.1lf = %.1lf", num1, num2, num1-num2);
+        break;
+        case '*':
+        printf("%.1lf * %.1lf = %.1lf", num1,num2, num1*num2);
+        break;
+        case '/':
+        if(num2 !=0.0){
+            printf("%.1lf / %.1lf = %.1lf", num1, num2, num1/num2);
+        }
+        else{
+            printf("Error! Division by Zero.");
+        }
+        break;
+        case '%':
+        if((int)num2 !=0){
+            printf("%d %% %d = %d", (int)num1, (int)num2, (int)num1%(int)num2);
+        }
+    else{
+        printf("Error! Division by zero.");
+    }
+    break;
+    default:
+    printf("Error! Operator is not correct");
+
+        }
+return 0;
+
+        }
+    
+
